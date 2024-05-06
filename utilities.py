0dc8ed9281ff802f5cd2ec5379fc6b0123037325
marionmckenzie <marion.mckenzie@mines.edu>
2024-05-06 18:32:20 +0000

    removal of extraneous code

--- utilities.py
+++ utilities.py
@@ -5,16 +5,6 @@ import pandas as pd
 def inputs_to_pandas(input_csv):
         
     csv = pd.read_csv(input_csv)
-    X = csv[['Topo', 'Bed', 'Elong', 'Area']] #input data that will be used to train the results
-    
-    X['Topo'][X['Topo'] == 'O']=1
-    X['Topo'][X['Topo'] == 'V']=0
-    X['Bed'][X['Bed'] == 'C']=1
-    X['Bed'][X['Bed'] == 'S']=0
-    
-    #X = X.astype('int8') #change all columns of the input dataset to integers from string
-    #add preprocess codes to one hot encode
-    
-    return X
+ 
 
 
