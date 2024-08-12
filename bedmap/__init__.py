2f7fca37fa107d7e415dabca06a41d1826e935f0
Ellie Abrahams <ellianna@berkeley.edu>
2024-08-12 14:32:21 -0700

    refactor: update script name

--- bedmap/__init__.py
+++ bedmap/__init__.py
@@ -1,18 +1,18 @@
 '''
-bedmap.
+bedfinder.
 
 This is initialized with the following modules:
-    - .bedmap      : Classify TPI products into glacially derived bedforms
+    - .bedfinder      : Classify TPI products into glacially derived bedforms
     - .filtering   : Filter input TPI data to improve model accuracy
     - .utilities   : Helper functions for the script
 
 '''
 
 __version__ = '0.0.1'
-__all__ = ['bedmap', 'filtering', 'utilities']
+__all__ = ['bedfinder', 'filtering', 'utilities']
 __author__ = 'elliesch <ellianna@berkeley.edu>', 'marionmckenzie <marion.mckenzie@mines.edu>'
 __minimum_python_version__ = '3.9'
 
-from .bedmap import *
+from .bedfinder import *
 from .filtering import *
 from .utilities import *
